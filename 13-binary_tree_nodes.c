98cf31325c2fc81f9417a457c741ff8e7c8c2142
imanebenhadda <imanebenhaddapama@gmail.com>
2024-01-04 23:04:18 +0100

    13 binary

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -15,11 +15,10 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 		return (0);
 	}
 
-	if (tree->right != NULL || tree->left != NULL)
+	if (!tree->left && !tree->right)
 	{
 		return (binary_tree_nodes(tree->left) +
 				binary_tree_nodes(tree->right) + 1);
 	}
 	return (0);
 }
-
