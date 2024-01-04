75a633d6cc6b92dbbdaed7c2e533d122f0270d27
imanebenhadda <imanebenhaddapama@gmail.com>
2024-01-04 23:37:35 +0100

    16binary

--- binary_trees.h
+++ binary_trees.h
@@ -41,4 +41,9 @@ size_t binary_tree_leaves(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
 int is_full_recursive(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
+size_t depth(const binary_tree_t *tree);
+unsigned char is_leaf(const binary_tree_t *node);
+const binary_tree_t *get_leaf(const binary_tree_t *tree);
+int binary_tree_is_perfect(const binary_tree_t *tree);
+int is_perfect_recursive(const binary_tree_t *tree, size_t leaf_depth, size_t level, size_t depth);
 #endif /*BINARY_TREE_H*/
